0afac3a72215c992d62de9ad8dec89881f0ace6e
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-06-03 03:51:20 -0300

    Controller e rotas - Problema de conexão com o mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/projetoultrassecreto', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/noderest');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
